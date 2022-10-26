272f8c3c09e608c3089c07ad1be096bd0887a2de
Luna <luna.devacc@gmail.com>
2022-10-26 12:03:22 +0900

    feat: entity, dto 추가

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ const mkdirOptions = {
   const moduleName = process.argv[2]
   const moduleRoot = `${SOURCE_ROOT}/${moduleName}`
 
-  const replaceModuleName = (moduleName, { type, parentPath }) => {
+  const replaceModuleName = (moduleName, { type, parentPath, fileNameFix }) => {
     const firstCharUpperCaseModuleName = moduleName.replace(/^[a-z]/, char => char.toUpperCase())
-    const fileName = `${moduleName}.${type}.ts`
+    const fileName = fileNameFix ? fileNameFix : `${moduleName}.${type}.ts`
     const path = type === 'module' ? `${moduleRoot}/${fileName}` : `${moduleRoot}/${parentPath}/${fileName}`
 
     fs.writeFileSync(
@@ -44,6 +44,7 @@ const mkdirOptions = {
     fs.mkdirSync(`${moduleRoot}/${DOMAIN_ROOT}/repository`, mkdirOptions)
     fs.mkdirSync(`${moduleRoot}/${INTERFACE_ADAPTER_ROOT}/dto`, mkdirOptions)
     fs.mkdirSync(`${moduleRoot}/${USE_CASE_ROOT}`, mkdirOptions)
+    fs.mkdirSync(`${moduleRoot}/${INFRA_ROOT}/repository`, mkdirOptions)
     fs.mkdirSync(`${moduleRoot}/${INFRA_ROOT}/entity`, mkdirOptions)
   } else console.error('\x1b[31m%s\x1b[0m', 'Error, usage: $mkdirca {root}')
 
@@ -51,6 +52,12 @@ const mkdirOptions = {
     { type: 'module', parentPath: '' },
     { type: 'controller', parentPath: INTERFACE_ADAPTER_ROOT },
     { type: 'service', parentPath: USE_CASE_ROOT },
+    { type: 'repository', parentPath: `${INFRA_ROOT}/repository` },
+    { type: 'entity', parentPath: `${INFRA_ROOT}/entity` },
+    { type: 'create', parentPath: `${INTERFACE_ADAPTER_ROOT}/dto`, fileNameFix: `create-${moduleName}.dto.ts` },
+    { type: 'update', parentPath: `${INTERFACE_ADAPTER_ROOT}/dto`, fileNameFix: `update-${moduleName}.dto.ts` },
+    { type: 'domain', parentPath: DOMAIN_ROOT, fixNameFix: `${moduleName}.ts` },
+    { type: 'iRepository', parentPath: `${DOMAIN_ROOT}/repository`, fileNameFix: `i${moduleName}.repository.ts` },
   ]
 
   MODULE_ELEMS.forEach(elem => replaceModuleName(moduleName, elem))
